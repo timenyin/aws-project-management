6fd008aa4f3db399ebbe0381b1605e85961b0ad6
Harmony <47876447+timenyin@users.noreply.github.com>
2024-11-09 18:37:37 +0100

    image to s3 bucket

--- mangement/next.config.mjs
+++ mangement/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "pm-s3-images-folder.s3.us-east-1.amazonaws.com",
+                port: "",
+                pathname: "/**",
+            }
+        ]
+    }
+};
 
-export default nextConfig;
+module.exports = nextConfig;
