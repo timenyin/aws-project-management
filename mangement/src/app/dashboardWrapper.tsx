f9f556c49579fbb70a4c1c1776f41af3deb911f3
Harmony <47876447+timenyin@users.noreply.github.com>
2024-11-11 11:02:31 +0100

    added resgisration form

--- mangement/src/app/dashboardWrapper.tsx
+++ mangement/src/app/dashboardWrapper.tsx
@@ -4,6 +4,7 @@
 import React, { useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import AuthProvider from "./authProvider";
 import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
@@ -40,7 +41,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
+      <AuthProvider>
       <DashboardLayout>{children}</DashboardLayout>
+      </AuthProvider>
     </StoreProvider>
   );
 };
--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -35,7 +35,12 @@ app.use("/search", searchRoutes_1.default);
 app.use("/users", userRoutes_1.default);
 app.use("/teams", teamRoutes_1.default);
 /* SERVER */
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on part ${port}`);
 });
+/* SERVER localhost:3000 only */
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server running on part ${port}`);
+// });
