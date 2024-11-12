2ec22b525880b5277dc72e58bd3f885f4f0d0ee6
Harmony <47876447+timenyin@users.noreply.github.com>
2024-11-12 12:25:47 +0100

    Update back to defult authProvider.tsx

--- mangement/src/app/authProvider.tsx
+++ mangement/src/app/authProvider.tsx
@@ -3,21 +3,14 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import "@aws-amplify/ui-react/styles.css";
 
-// Amplify.configure({
-//   Auth: {
-//     Cognito: {
-//       userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
-//       userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
-//     },
-//   },
-// });
-
 Amplify.configure({
-    Auth: {
+  Auth: {
+    Cognito: {
       userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
-      userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
+      userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
     },
-  });
+  },
+});
 
 const formFields = {
   signUp: {
